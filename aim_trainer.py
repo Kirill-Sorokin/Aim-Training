94f0fdd7e7999f895057c9b39ec439b2393b095d
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-24 17:08:30 +0000

    screen size setup

--- aim_trainer.py
+++ aim_trainer.py
@@ -1,1 +1,11 @@
-# test commit
+import math
+import random
+import time
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 600, 400
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aim Training")
+
