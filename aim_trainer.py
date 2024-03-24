f6f4b5bfb3af067b0075f0b08764ebbe4e5c96a1
Kirill David Sorokin <96061027+Kirill-Sorokin@users.noreply.github.com>
2024-03-24 17:40:02 +0000

    create & add detail to targets

--- aim_trainer.py
+++ aim_trainer.py
@@ -12,6 +12,7 @@ class Target:
     MAX_SIZE = 30
     GROWTH_RATE = 0.2
     COLOR = "red"
+    SECOND_COLOR = "white"
 
     def __init__(self, x, y):
         self.x = x
@@ -27,6 +28,14 @@ class Target:
             self.size += self.GROWTH_RATE
         else:
             self.size -= self.GROWTH_RATE
+
+    # add detail to circles (targets) (they overlap)
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.2)
 
 
 def main():
